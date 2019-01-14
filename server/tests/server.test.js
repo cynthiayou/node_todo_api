1c5b234df28dc495066830a3907a557e30f85a58
cynthiayou <yxqcynthia@gmail.com>
2019-01-13 20:57:36 -0600

    Add GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -43,6 +43,7 @@ describe('POST /todos', () => {
                 if (err){
                     return done(err);
                 }
+                
                 Todo.find().then((todos) => {
                     expect(todos.length).toBe(0);
                     done();
