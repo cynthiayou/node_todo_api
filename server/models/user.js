bc27cb8354ece390e65293d167f4efec3d10ee42
cynthiayou <yxqcynthia@gmail.com>
2019-01-19 22:05:55 -0600

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,35 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  let User = this;
+  return User.findOne({email}).then((user) => {
+    if (!user){
+      return Promise.reject();
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+              resolve(user);
+          }else{
+              reject();
+          }
+      })
+    })
+  })
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  };
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
 UserSchema.pre('save', function (next) {
   let user = this;
   if (user.isModified('password')) {
